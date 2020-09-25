6510c27e8bdc2fe16591847d42634565fc42f5ef
lucasbdias <lucasbrzdias@gmail.com>
2020-09-24 21:59:11 -0300

    Tweet component finished

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Feed from '../Feed';
+
 import {
   Container,
   Banner,
@@ -7,7 +9,8 @@ import {
   ProfileData,
   LocationIcon,
   CakeIcon,
-  Followage
+  Followage,
+  EditButton
 } from './styles';
 
 const ProfilePage: React.FC = () => {
@@ -17,7 +20,7 @@ const ProfilePage: React.FC = () => {
         <Avatar />
       </Banner>
       <ProfileData>
-          {/* <EditButton outlined>Editar Perfil</EditButton> */}
+          <EditButton outlined>Editar Perfil</EditButton>
           <h1>Lucas Braz Dias</h1>
           <h2>@lucasbdias</h2>
 
@@ -46,6 +49,8 @@ const ProfilePage: React.FC = () => {
             </span>
           </Followage>          
         </ProfileData>
+
+        <Feed />
     </Container>
   );
 }
--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -1,6 +1,7 @@
 import styled, {css} from 'styled-components';
 
 import { LocationOn, Cake } from "../../styles/icons";
+import Button from '../Button';
 
 export const Container = styled.div`
   display: flex;
@@ -92,4 +93,30 @@ const iconCss = css`
 export const LocationIcon = styled(LocationOn)`${iconCss}`;
 export const CakeIcon = styled(Cake)`${iconCss}`;
 
-export const Followage = styled.div``;
+export const Followage = styled.div`
+  display: flex;
+
+  > span {
+    font-size: 15px;
+    color: var(--gray);
+
+    & + span {
+      margin-left: 20px;
+    }
+  }
+`;
+
+export const EditButton = styled(Button)`
+  position: absolute;
+  top: 2vw;
+  right: 7px;
+
+  padding: 4px 16px;
+  font-size: 13px;
+
+  @media (min-width: 320px) {
+    top: 10px;
+    padding: 10px 19px;
+    font-size: 15px;
+  }
+`;
